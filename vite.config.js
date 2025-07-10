d180584a2a9c44f8b03164f9b653207c511b7623
王锦辉 <1569810815@qq.com>
2025-07-10 09:28:22 +0800

    路径配置

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/jingya/',
+  build: {
+    outDir: 'docs', // 指定输出目录
+    assetsDir: 'assets', // 静态资源目录
+    emptyOutDir: true // 构建前清空输出目录
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
