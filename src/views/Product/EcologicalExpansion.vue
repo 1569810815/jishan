f1160dcfabd9627864f0630d097e4b304eaaa9cb
王锦辉 <1569810815@qq.com>
2025-07-31 10:57:23 +0800

    积善网站1.0

--- src/views/Product/EcologicalExpansion.vue
+++ src/views/Product/EcologicalExpansion.vue
@@ -18,6 +18,14 @@
       </div>
     </div>
   </div>
+  <div class="processbg">
+    <h2>依据养老机构角色"核心痛点"针对性研发</h2>
+    <ul>
+      <li v-for="(item, index) in processList" :key="index">
+        <span>{{ item.title }}</span>
+      </li>
+    </ul>
+  </div>
   <ToTop @go-first="scrollToTop" />
   <Consultation></Consultation>
   <Footer></Footer>
@@ -49,20 +57,35 @@ const EntityItemsList = ref([
     image:anli
   }
 ])
+const processList = ref([
+  {title:'缺乏获客入口开放能力'},
+  {title:'各部门协作效率低'},
+  {title:'预警不及时运营风险高'},
+  {title:'年检资料准备耗时费力'},
+  {title:'排版混乱/运力不畅'},
+  {title:'年纪较长/学习难度高'},
+  {title:'粗放式管理/运营成本高'},
+  {title:'信息碎片化/服务不透明'},
+  {title:'运营汇报'},
+  {title:'运营汇报'},
+  {title:'家属口碑'},
+  {title:'铸就品牌'},
+
+])
 </script>
 
 <style scoped>
 .Entity-items {
   max-width: 1200px;
   margin: 0 auto;
-  padding: 100px 0;
+  padding: 8vh 0;
 }
 
 .entity-item {
   display: flex;
   align-items:flex-start;
-  margin-bottom: 6.25rem;
-  gap: 100px;
+  margin-bottom: 5vh;
+  gap: 6vh;
 }
 
 .entity-item.reverse {
@@ -78,11 +101,11 @@ const EntityItemsList = ref([
   font-weight: bold;
   font-size: 1.6vw;
   color: #ef203a;
-  margin: 3rem 0;
+  margin: 3vh 0;
 }
 
 .text-content p {
-  font-size: 16px;
+  font-size: .8vw;
   line-height: 1.8;
   color: #666;
 }
@@ -97,7 +120,104 @@ const EntityItemsList = ref([
   border-radius: 8px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
 }
+.processbg {
+  width: 100%;
+  margin: 0 auto;
+  background-image: url('@/assets/Images/anlibg2.png');
+  background-size: cover;
+  background-position: center;
+  padding: 8vh 0;
+}
+.processbg h2 {
+  font-family: "Alimama ShuHei", sans-serif;
+  font-weight: bold;
+  font-size: 1.8vw;
+  color: #000;
+  text-align: center;
+}
+.processbg ul {
+  position: relative;
+  max-width: 1200px;
+  background: url('@/assets/Images/processbg.png') no-repeat center center;
+  background-size: 100% auto;
+  min-height: 700px;
+  margin: 0 auto;
+  list-style: none;
+  padding: 0;
+}
 
+.processbg ul li {
+  position: absolute;
+  font-family: "Alimama ShuHei", sans-serif;
+  font-weight: bold;
+  transform: translate(-50%, -50%);
+  white-space: nowrap;
+  cursor: pointer;
+  transition: all 0.3s ease;
+    color: #000;
+  font-size: 0.6vw;
+}
+.processbg ul li:nth-child(1) {
+  top: 10%;
+  left: 40%;
+}
+
+.processbg ul li:nth-child(2) {
+  top: 10%;
+  left: 60%;
+}
+.processbg ul li:nth-child(3) {
+  top: 43%;
+  left: 60%;
+}
+.processbg ul li:nth-child(4) {
+  top: 43%;
+  left: 40%;
+}
+.processbg ul li:nth-child(5) {
+  top: 67%;
+  left: 40%;
+}
+.processbg ul li:nth-child(6) {
+  top: 67%;
+  left: 60%;
+}
+.processbg ul li:nth-child(7) {
+  top: 34.5%;
+  left: 11.5%;
+}
+.processbg ul li:nth-child(8) {
+  top: 34.5%;
+  left: 88.5%;
+}
+.processbg ul li:nth-child(9) {
+  transform: rotate(-32deg);
+  font-size: 1.4vw;
+  color: #ef203a;
+  top: 30%;
+  left: 25%;
+}
+.processbg ul li:nth-child(10) {
+  transform: rotate(32deg);
+  font-size: 1.4vw;
+  color: #ef203a;
+  top: 30%;
+  left: 63%;
+}
+.processbg ul li:nth-child(11) {
+  transform: rotate(32deg);
+  font-size: 1.4vw;
+  color: #ef203a;
+  top: 72%;
+  left: 24%;
+}
+.processbg ul li:nth-child(12) {
+  transform: rotate(-32deg);
+  font-size: 1.4vw;
+  color: #ef203a;
+  top: 72%;
+  left: 64%;
+}
 @media (max-width: 768px) {
   .entity-item {
     flex-direction: column;
